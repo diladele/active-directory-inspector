246d409b451384a806bf40d12c360a30733377f1
Rafael Akchurin <rafael.akchurin@diladele.lan>
2017-10-11 10:22:32 +0200

    added simple test

---
+++ src/Diladele.ActiveDirectory.Inspection.Test/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Diladele.ActiveDirectory.Inspection.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestEventLogListener();
+            TestProber();
+        }
+
+        static void TestEventLogListener()
+        {
+            var listener = new EventLogListener();
+            while (true)
+            {
+                // wait 
+                Thread.Sleep(5000);
+
+                // get the events accumulated
+                foreach(InfoBase info in listener.GetEvents())
+                {
+                    Console.WriteLine(info);
+                }
+            }
+        }
+
+        static void TestProber()
+        {
+            Prober prober = new Prober("192.168.1.103");
+            {
+                List<ProbedInfo> probes = prober.Probe();
+                foreach (ProbedInfo info in probes)
+                {
+                    Console.WriteLine(info.Domain);
+                    Console.WriteLine(info.UserName);
+                    Console.WriteLine(info.UserSid);
+                }
+            }
+        }
+
+
+    }
+}
