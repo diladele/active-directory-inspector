319ecb509a7e161f432db57dbc212ad14e493a84
Rafael Akchurin <rafael.akchurin@diladele.lan>
2017-10-13 17:13:06 +0200

    intermediate checkin

--- src/Diladele.ActiveDirectory.Inspection.Test/Program.cs
+++ src/Diladele.ActiveDirectory.Inspection.Test/Program.cs
@@ -26,61 +26,94 @@ namespace Diladele.ActiveDirectory.Inspection.Test
             Trace.Listeners.Add(new TextWriterTraceListener(GetDiskPath()));
             Trace.AutoFlush = true;
 
-            //TestHarvester();
-            TestInspector();
-            //TestEventLogListener();
+            TestHarvester();
+            //TestListener();
+
+            //TestInspector();            
             //TestProber();
         }
 
 
         static void TestHarvester()
         {
-            foreach(Workstation w in Harvester.Harvest())
+            using (var harvester = new Harvester())
             {
-                Console.WriteLine(w.DnsHostName);
+                // wait 10 seconds
+                Thread.Sleep(10000);
+
+                // there should be some workstations as first time harvester runs instantly
+                List<Workstation> workstations = harvester.GetWorkstations();
+                foreach (var workstation in workstations)
+                {
+                    Console.WriteLine(workstation.DnsHostName);
+                }
+
+                // wait another 10 seconds
+                Thread.Sleep(10000);
+
+                // debug check there should not be any workstations as the next harvest will take place in a minute
+                workstations = harvester.GetWorkstations();
+                Debug.Assert(workstations == null || workstations.Count == 0);
             }
         }
 
         static void TestInspector()
         {
-            using(var inspector = new Inspector())
+            // load storage from disk
+            var storage = StorageFactory.LoadFromDisk();
+
+            // create listener
+            var listener = new Listener(storage);
+
+            // wait 10 seconds
+            Thread.Sleep(10000);
+
+            /*
+
+            // create harvester
+            using(var harvester = new Harvester())
             {
-                for (var i = 0; i < 1000; i++)
+                // and finally inspector
+                using (var inspector = new Inspector(storage, harvester, listener))
                 {
-                    // wait 
-                    Thread.Sleep(10000);
+                    for (var i = 0; i < 1000; i++)
+                    {
+                        // wait 
+                        Thread.Sleep(10000);
+                    }
                 }
             }
+             * 
+             * */
         }
 
-        static void TestEventLogListener()
+        static void TestListener()
         {
-            var listener = new Listener();
-            while (true)
+            // load storage from disk
+            var storage = StorageFactory.LoadFromDisk();
+
+            // create listener
+            var listener = new Listener(storage);
+
+            while(true)
             {
                 // wait 
                 Thread.Sleep(5000);
-
-                // get the events accumulated
-                foreach(Activity info in listener.GetActivities())
-                {
-                    Console.WriteLine(info);
-                }
             }
         }
 
         static void TestProber()
         {
+            /*
             var ip = IPAddress.Parse("192.168.1.103");
 
-            List<User> users = UserProber.Probe(ip);
+            List<User> users = Prober.Probe(ip);
             foreach (var user in users)
             {
                 Console.WriteLine(user.Domain);
                 Console.WriteLine(user.Name);
                 Console.WriteLine(user.Sid);
-            }
-            
+            }*/            
         }
     }
 }
