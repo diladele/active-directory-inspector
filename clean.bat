917814889c06020bb45cf8673f8c589c50ba1f37
Rafael Akchurin <rafael.akchurin@diladele.lan>
2017-10-17 13:25:42 +0200

    better clean

--- clean.bat
+++ clean.bat
@@ -1,3 +1,17 @@
 :: remove bin and ztmp folders
 rmdir /S /Q .\bin
 rmdir /S /Q .\ztmp
+
+:: remove all output of .net projects
+rmdir /S /Q .\src\Diladele.ActiveDirectory.Inspection\bin
+rmdir /S /Q .\src\Diladele.ActiveDirectory.Inspection\obj
+rmdir /S /Q .\src\Diladele.ActiveDirectory.Server\bin
+rmdir /S /Q .\src\Diladele.ActiveDirectory.Server\obj
+rmdir /S /Q .\src\Diladele.ActiveDirectory.Service\bin
+rmdir /S /Q .\src\Diladele.ActiveDirectory.Service\obj
+
+:: remove all output of .net test projects
+rmdir /S /Q .\src.test\Diladele.ActiveDirectory.Inspection.Test\bin
+rmdir /S /Q .\src.test\Diladele.ActiveDirectory.Inspection.Test\obj
+rmdir /S /Q .\src.test\Diladele.ActiveDirectory.Inspection.UI\bin
+rmdir /S /Q .\src.test\Diladele.ActiveDirectory.Inspection.UI\obj
